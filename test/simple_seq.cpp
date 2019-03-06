ea8c33a6ded2d5151992ebcede7fc9463eeb93be
Joao Paulo Magalhaes <dev@jpmag.me>
2019-03-06 02:31:12 +0000

    fix special characters on sequences

--- test/simple_seq.cpp
+++ test/simple_seq.cpp
@@ -6,12 +6,30 @@ namespace yml {
 #define SIMPLE_SEQ_CASES                                \
 "simple seq",                                           \
 "simple seq, explicit, single line",                    \
+"simple seq, explicit, single line, trailcomma",        \
 "simple seq, explicit, multiline, unindented",          \
+"simple seq, explicit, multiline, unindented, trailcomma", \
 "simple seq, explicit, multiline, comments inline",     \
 "simple seq, explicit, multiline, comments prev line",  \
 "simple seq, explicit, multiline, indented",            \
 "simple seq, comments inline",                          \
-"simple seq, comments prev line"
+"simple seq, comments prev line",                       \
+"simple seq, scalars with special chars, comma",            \
+"simple seq, scalars with special chars, semicolon",        \
+"simple seq, scalars with special chars, cardinal",         \
+"simple seq, scalars with special chars, dash",             \
+"simple seq, scalars with special chars, left-bracket",     \
+"simple seq, scalars with special chars, right-bracket",  \
+"simple seq, scalars with special chars, left-curly",       \
+"simple seq, scalars with special chars, right-curly",      \
+"simple seq expl, scalars with special chars, comma",            \
+"simple seq expl, scalars with special chars, semicolon",            \
+"simple seq expl, scalars with special chars, cardinal",            \
+"simple seq expl, scalars with special chars, dash",            \
+"simple seq expl, scalars with special chars, left-bracket",            \
+"simple seq expl, scalars with special chars, right-bracket",            \
+"simple seq expl, scalars with special chars, left-curly",            \
+"simple seq expl, scalars with special chars, right-curly"            \
 
 
 CASE_GROUP(SIMPLE_SEQ)
@@ -24,13 +42,18 @@ R"(- 0
 - 2
 - 3
 )",
-    L{N{"0"}, N{"1"}, N{"2"}, N{"3"}}
+L{N{"0"}, N{"1"}, N{"2"}, N{"3"}}
 ),
 
 
 C("simple seq, explicit, single line",
 "[0, 1, 2, 3]",
-    L{N{"0"}, N{"1"}, N{"2"}, N{"3"}}
+L{N{"0"}, N{"1"}, N{"2"}, N{"3"}}
+),
+
+C("simple seq, explicit, single line, trailcomma",
+"[0, 1, 2, 3,]",
+L{N{"0"}, N{"1"}, N{"2"}, N{"3"},}
 ),
 
 C("simple seq, explicit, multiline, unindented",
@@ -39,6 +62,16 @@ R"([
 1,
 2,
 3
+])",
+    L{N{"0"}, N{"1"}, N{"2"}, N{"3"}}
+),
+
+C("simple seq, explicit, multiline, unindented, trailcomma",
+R"([
+0,
+1,
+2,
+3,
 ])",
     L{N{"0"}, N{"1"}, N{"2"}, N{"3"}}
 ),
@@ -99,6 +132,330 @@ R"(
 - 3
 )",
     L{N{"0"}, N{"1"}, N{"2"}, N{"3"}}
+),
+
+C("simple seq, scalars with special chars, comma",
+R"(
+- a,b
+- c,d
+- e,f
+- a, b
+- c, d
+- e, f
+- a , b
+- c , d
+- e , f
+- a ,b
+- c ,d
+- e ,f
+)",
+L{N{"a,b"}, N{"c,d"}, N{"e,f"},
+  N{"a, b"}, N{"c, d"}, N{"e, f"},
+  N{"a , b"}, N{"c , d"}, N{"e , f"},
+  N{"a ,b"}, N{"c ,d"}, N{"e ,f"},
+    }
+),
+
+C("simple seq, scalars with special chars, semicolon",
+R"(
+- a:b
+- "c:d"
+- 'e:f'
+- a :b
+- "c :d"
+- 'e :f'
+- a : b    # THIS IS A KEY-VAL!!!
+- "c : d"
+- 'e : f'
+- a: b     # THIS IS A KEY-VAL!!!
+- "c: d"
+- 'e: f'
+)",
+L{
+  N("a:b"), N("c:d"), N("e:f"),
+  N("a :b"), N("c :d"), N("e :f"),
+  N(L{N("a", "b")}), N("c : d"), N("e : f"),
+  N(L{N("a", "b")}), N("c: d"), N("e: f"),
+    }
+),
+
+C("simple seq, scalars with special chars, cardinal",
+R"(
+- a#b
+- "a#b"
+- 'a#b'
+- a# b
+- "a# b"
+- 'a# b'
+- a # b
+- "a # b"
+- 'a # b'
+- a #b
+- "a #b"
+- 'a #b'
+)",
+L{
+  N{"a#b"}, N{"a#b"}, N{"a#b"},
+  N{"a# b"}, N{"a# b"}, N{"a# b"},
+  N{"a"}, N{"a # b"}, N{"a # b"},
+  N{"a"}, N{"a #b"}, N{"a #b"},
+    }
+),
+
+C("simple seq, scalars with special chars, dash",
+R"(
+- a-b
+- "a-b"
+- 'a-b'
+- a- b
+- "a- b"
+- 'a- b'
+- a - b
+- "a - b"
+- 'a - b'
+- a -b
+- "a -b"
+- 'a -b'
+)",
+L{
+  N{"a-b"}, N{"a-b"}, N{"a-b"},
+  N{"a- b"}, N{"a- b"}, N{"a- b"},
+  N{"a - b"}, N{"a - b"}, N{"a - b"},
+  N{"a -b"}, N{"a -b"}, N{"a -b"},
+    }
+),
+
+C("simple seq, scalars with special chars, left-curly",
+R"(
+- a{b
+- "a{b"
+- 'a{b'
+- a{ b
+- "a{ b"
+- 'a{ b'
+- a { b
+- "a { b"
+- 'a { b'
+- a {b
+- "a {b"
+- 'a {b'
+)",
+L{
+  N{"a{b"}, N{"a{b"}, N{"a{b"},
+  N{"a{ b"}, N{"a{ b"}, N{"a{ b"},
+  N{"a { b"}, N{"a { b"}, N{"a { b"},
+  N{"a {b"}, N{"a {b"}, N{"a {b"},
+    }
+),
+
+C("simple seq, scalars with special chars, right-curly",
+R"(
+- a}b
+- "a}b"
+- 'a}b'
+- a} b
+- "a} b"
+- 'a} b'
+- a } b
+- "a } b"
+- 'a } b'
+- a }b
+- "a }b"
+- 'a }b'
+)",
+L{
+  N{"a}b"}, N{"a}b"}, N{"a}b"},
+  N{"a} b"}, N{"a} b"}, N{"a} b"},
+  N{"a } b"}, N{"a } b"}, N{"a } b"},
+  N{"a }b"}, N{"a }b"}, N{"a }b"},
+    }
+),
+
+C("simple seq, scalars with special chars, left-bracket",
+R"(
+- a[b
+- "a[b"
+- 'a[b'
+- a[ b
+- "a[ b"
+- 'a[ b'
+- a [ b
+- "a [ b"
+- 'a [ b'
+- a [b
+- "a [b"
+- 'a [b'
+)",
+L{
+  N{"a[b"}, N{"a[b"}, N{"a[b"},
+  N{"a[ b"}, N{"a[ b"}, N{"a[ b"},
+  N{"a [ b"}, N{"a [ b"}, N{"a [ b"},
+  N{"a [b"}, N{"a [b"}, N{"a [b"},
+    }
+),
+
+C("simple seq, scalars with special chars, right-bracket",
+R"(
+- a]b
+- "a]b"
+- 'a]b'
+- a] b
+- "a] b"
+- 'a] b'
+- a ] b
+- "a ] b"
+- 'a ] b'
+- a ]b
+- "a ]b"
+- 'a ]b'
+)",
+L{
+  N{"a]b"}, N{"a]b"}, N{"a]b"},
+  N{"a] b"}, N{"a] b"}, N{"a] b"},
+  N{"a ] b"}, N{"a ] b"}, N{"a ] b"},
+  N{"a ]b"}, N{"a ]b"}, N{"a ]b"},
+    }
+),
+
+C("simple seq expl, scalars with special chars, comma",
+R"([
+ a,b,  "c,d",   'e,f',
+ a, b,  "c, d",   'e, f',
+ a , b,  "c , d",   'e , f',
+ a ,b,  "c ,d",   'e ,f',
+])",
+L{
+  N{"a"}, N("b"), N("c,d"), N("e,f"),
+  N{"a"}, N("b"), N("c, d"), N("e, f"),
+  N{"a"}, N("b"), N("c , d"), N("e , f"),
+  N{"a"}, N("b"), N("c ,d"), N("e ,f"),
+    }
+),
+
+C("simple seq expl, scalars with special chars, semicolon",
+R"([
+# a:b,  # not legal
+  "c:d",   'e:f',
+# a: b,  # not legal
+  "c: d",   'e: f',
+# a : b,  # not legal
+  "c : d",   'e : f',
+# a :b,  # not legal
+  "c :d",   'e :f',
+])",
+L{/*...not legal...*/
+  /*N{"a"}, N("b"),*/ N("c:d"), N("e:f"),
+  /*N{"a"}, N("b"),*/ N("c: d"), N("e: f"),
+  /*N{"a"}, N("b"),*/ N("c : d"), N("e : f"),
+  /*N{"a"}, N("b"),*/ N("c :d"), N("e :f"),
+    }
+),
+
+C("simple seq expl, scalars with special chars, cardinal",
+R"([
+ a#b, "c#d",   'e#f',
+ a# b, "c# d",   'e# f',
+ a # b, "c # d",   'e # f',
+, # this is needed because of the comment above
+ a #b, "c #d",   'e #f',
+])",
+L{
+  N{"a#b"}, N("c#d"), N("e#f"),
+  N{"a# b"}, N("c# d"), N("e# f"),
+  N{"a"},
+  N{"a"},
+    }
+),
+
+C("simple seq expl, scalars with special chars, dash",
+R"([
+ a-b, "c-d",   'e-f',
+ a- b, "c- d",   'e- f',
+ a - b, "c - d",   'e - f',
+ a -b, "c -d",   'e -f',
+])",
+L{
+  N{"a-b"}, N("c-d"), N("e-f"),
+  N{"a- b"}, N("c- d"), N("e- f"),
+  N{"a - b"}, N("c - d"), N("e - f"),
+  N{"a -b"}, N("c -d"), N("e -f"),
+    }
+),
+
+C("simple seq expl, scalars with special chars, left-bracket",
+R"([
+# a[b,
+   "c[d",   'e[f',
+# a[ b,
+   "c[ d",   'e[ f',
+# a [ b,
+   "c [ d",   'e [ f',
+# a [b,
+   "c [d",   'e [f',
+])",
+L{
+  /*N{"a[b"}, */N("c[d"), N("e[f"),
+  /*N{"a[ b"}, */N("c[ d"), N("e[ f"),
+  /*N{"a [ b"},*/ N("c [ d"), N("e [ f"),
+  /*N{"a [b"}, */N("c [d"), N("e [f"),
+    }
+),
+
+C("simple seq expl, scalars with special chars, right-bracket",
+R"([
+# a]b,
+   "c]d",   'e]f',
+# a] b,
+   "c] d",   'e] f',
+# a ] b,
+   "c ] d",   'e ] f',
+# a ]b,
+   "c ]d",   'e ]f',
+])",
+L{
+  /*N{"a]b"}, */N("c]d"), N("e]f"),
+  /*N{"a] b"}, */N("c] d"), N("e] f"),
+  /*N{"a ] b"},*/ N("c ] d"), N("e ] f"),
+  /*N{"a ]b"}, */N("c ]d"), N("e ]f"),
+    }
+),
+
+C("simple seq expl, scalars with special chars, left-curly",
+R"([
+# a{b,
+   "c{d",   'e{f',
+# a{ b,
+   "c{ d",   'e{ f',
+# a { b,
+   "c { d",   'e { f',
+# a {b,
+   "c {d",   'e {f',
+])",
+L{
+  /*N{"a{b"}, */N("c{d"), N("e{f"),
+  /*N{"a{ b"}, */N("c{ d"), N("e{ f"),
+  /*N{"a { b"},*/ N("c { d"), N("e { f"),
+  /*N{"a {b"}, */N("c {d"), N("e {f"),
+    }
+),
+
+C("simple seq expl, scalars with special chars, right-curly",
+R"([
+# a}b,
+   "c}d",   'e}f',
+# a} b,
+   "c} d",   'e} f',
+# a } b,
+   "c } d",   'e } f',
+# a }b,
+   "c }d",   'e }f',
+])",
+L{
+  /*N{"a}b"}, */N("c}d"), N("e}f"),
+  /*N{"a} b"}, */N("c} d"), N("e} f"),
+  /*N{"a } b"},*/ N("c } d"), N("e } f"),
+  /*N{"a }b"}, */N("c }d"), N("e }f"),
+    }
 ),
     )
 }
