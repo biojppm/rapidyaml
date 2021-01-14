27d5c654e60e64a4f7ea15fcf07601b57dd5208b
Keith Rothman <537074+litghost@users.noreply.github.com>
2021-01-14 12:57:43 -0800

    Don't require cmake_component.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- api/python/setup.py
+++ api/python/setup.py
@@ -4,13 +4,21 @@ from setuptools import setup
 from cmake_build_extension import BuildExtension, CMakeExtension
 
 # define a CMake package
-ext = CMakeExtension('ryml.ryml',
-                     install_prefix='',
-                     source_dir=str(Path("../../").absolute()),
-                     cmake_component='python',
-                     cmake_configure_options=[
-                        "-DRYML_BUILD_API:BOOL=ON",
-                        ])
+cmake_args = dict(
+    name='ryml.ryml',
+    install_prefix='',
+    source_dir=str(Path("../../").absolute()),
+    cmake_component='python',
+    cmake_configure_options=[
+        "-DRYML_BUILD_API:BOOL=ON",
+        ])
+
+try:
+    ext = CMakeExtension(**cmake_args)
+except TypeError:
+    # FIXME: cmake_build_extension may not support cmake_component.
+    del cmake_args['cmake_component']
+    ext = CMakeExtension(**cmake_args)
 
 setup(name='rapidyaml',
     version='1.0',
