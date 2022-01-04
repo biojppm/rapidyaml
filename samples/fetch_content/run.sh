250c9c52d1212f151371dee72351acce2e9c7553
Joao Paulo Magalhaes <dev@jpmag.me>
2022-01-04 17:38:38 +0000

    [ci] fix fetch_content sample to use same branch in the CI

--- samples/fetch_content/run.sh
+++ samples/fetch_content/run.sh
@@ -2,9 +2,11 @@
 
 # take the build type from the command line, or default to release
 cfg=${1:-Release}
+# force cmake's FetchContent to choose a specific branch, or default to nothing
+branch=${2:-}
 # make sure to run from where this file is
 cd $(dirname $0)
 # configure the sample
-cmake -S . -B ./build/$cfg -DCMAKE_BUILD_TYPE=$cfg
+cmake -S . -B ./build/$cfg -DCMAKE_BUILD_TYPE=$cfg -DRYML_BRANCH_NAME="$branch"
 # build and run the sample
 cmake --build ./build/$cfg --config $cfg --target run
