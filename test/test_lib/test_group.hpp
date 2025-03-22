d8d3ec7058d2653a14509189c555ec7e19b0c7ab
Joao Paulo Magalhaes <dev@jpmag.me>
2025-03-22 20:57:30 +0000

    refactor error API: update tests

--- test/test_lib/test_group.hpp
+++ test/test_lib/test_group.hpp
@@ -194,8 +194,8 @@ struct CaseAdderGcc4_8
     const int line;
 
     template<typename... Args>
-    void operator ()(Args... parameters) const {
-        group_cases->emplace_back(csubstr(file), line, parameters...);
+    void operator ()(Args &&... parameters) const {
+        group_cases->emplace_back(csubstr(file), line, std::forward<Args>(parameters)...);
     }
 };
 
