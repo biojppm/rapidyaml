f6d33520ee3c03fd046d905698bfcfabc1d0f570
Joao Paulo Magalhaes <dev@jpmag.me>
2025-03-30 02:38:51 +0100

    wip [ci skip]

--- ext/testbm.cmake
+++ ext/testbm.cmake
@@ -1,4 +1,4 @@
-
+# these are used both for testing and benchmarking
 set(C4FS_DIR ${CMAKE_CURRENT_BINARY_DIR}/subprojects/c4fs)
 c4_download_remote_proj(c4fs C4FS_DIR
     GIT_REPOSITORY https://github.com/biojppm/c4fs
@@ -8,10 +8,25 @@ c4_add_library(c4fs
         ${C4FS_DIR}/src/c4/fs/fs.hpp
         ${C4FS_DIR}/src/c4/fs/fs.cpp
     INC_DIRS
-        ${CMAKE_CURRENT_LIST_DIR}/c4core/src
         ${C4FS_DIR}/src
+        ${CMAKE_CURRENT_LIST_DIR}/c4core.src
+        ${CMAKE_CURRENT_LIST_DIR}/c4core.dev
     LIBS
         ryml
-    FOLDER
-        ext
+)
+
+set(C4LOG_DIR ${CMAKE_CURRENT_BINARY_DIR}/subprojects/c4log)
+c4_download_remote_proj(c4log C4LOG_DIR
+    GIT_REPOSITORY https://github.com/biojppm/c4log
+    GIT_TAG master)
+c4_add_library(c4log
+    SOURCES
+        ${C4LOG_DIR}/src/c4/log/log.hpp
+        ${C4LOG_DIR}/src/c4/log/log.cpp
+    INC_DIRS
+        ${C4LOG_DIR}/src
+        ${CMAKE_CURRENT_LIST_DIR}/c4core.src
+        ${CMAKE_CURRENT_LIST_DIR}/c4core.dev
+    LIBS
+        ryml
 )
