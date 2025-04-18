e7794d5ac3b22059e2847aff21642d70987069f9
Joao Paulo Magalhaes <dev@jpmag.me>
2025-04-19 00:48:43 +0100

    wip

--- ext/testbm.cmake
+++ ext/testbm.cmake
@@ -3,30 +3,26 @@ set(C4FS_DIR ${CMAKE_CURRENT_BINARY_DIR}/subprojects/c4fs)
 c4_download_remote_proj(c4fs C4FS_DIR
     GIT_REPOSITORY https://github.com/biojppm/c4fs
     GIT_TAG master)
-add_library(c4fs)
-target_sources(c4fs
-    PUBLIC ${C4FS_DIR}/src/c4/fs/fs.hpp
-    PRIVATE ${C4FS_DIR}/src/c4/fs/fs.cpp)
-target_include_directories(c4fs PUBLIC
-    ${C4FS_DIR}/src
-    ${CMAKE_CURRENT_LIST_DIR}/c4core.src
-    ${CMAKE_CURRENT_LIST_DIR}/c4core.dev
+c4_add_library(c4fs
+    SOURCES
+        ${C4FS_DIR}/src/c4/fs/fs.hpp
+        ${C4FS_DIR}/src/c4/fs/fs.cpp
+    INC_DIRS
+        ${C4FS_DIR}/src
+    LIBS
+        ryml
 )
 
 set(C4LOG_DIR ${CMAKE_CURRENT_BINARY_DIR}/subprojects/c4log)
 c4_download_remote_proj(c4log C4LOG_DIR
     GIT_REPOSITORY https://github.com/biojppm/c4log
     GIT_TAG master)
-add_library(c4log)
-target_sources(c4log
-    PUBLIC
+c4_add_library(c4log
+    SOURCES
         ${C4LOG_DIR}/src/c4/log/log.hpp
-    PRIVATE
         ${C4LOG_DIR}/src/c4/log/log.cpp
-        ${CMAKE_CURRENT_LIST_DIR}/c4core.dev/c4/memory_resource.hpp
-        ${CMAKE_CURRENT_LIST_DIR}/c4core.dev/c4/memory_resource.cpp)
-target_include_directories(c4log PUBLIC
-    ${C4LOG_DIR}/src
-    ${CMAKE_CURRENT_LIST_DIR}/c4core.src
-    ${CMAKE_CURRENT_LIST_DIR}/c4core.dev
+    INC_DIRS
+        ${C4LOG_DIR}/src
+    LIBS
+        ryml
 )
