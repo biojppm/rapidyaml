876376d3c699def39da62a5de660368ca741d853
Joao Paulo Magalhaes <dev@jpmag.me>
2026-01-22 19:37:48 +0000

    wip dev as lib

--- ext/c4core.cmake
+++ ext/c4core.cmake
@@ -1,15 +1,27 @@
 if(NOT _ext_c4core_cmake_included)
 set(_ext_c4core_cmake_included ON)
 
-set(_ryml_ext_dir ${CMAKE_CURRENT_LIST_DIR})  # see https://stackoverflow.com/questions/12802377/in-cmake-how-can-i-find-the-directory-of-an-included-file
 
 function(ryml_add_c4core_src_to_target target)
     _ryml_add_c4core_to_target(${target} c4core.src ON)
 endfunction()
 
+
 function(ryml_add_c4core_dev_to_target target)
-    _ryml_add_c4core_to_target(${target} c4core.dev OFF)
+    if(NOT TARGET ryml-_c4core_dev)
+        add_library(ryml-_c4core_dev)
+        _ryml_add_c4core_to_target(ryml-_c4core_dev c4core.dev OFF)
+        target_include_directories(ryml-_c4core_dev PUBLIC
+            ${_ryml_ext_dir}/c4core.src
+            ${_ryml_ext_dir}/c4core.dev
+        )
+    endif()
+    target_link_libraries(${target} PUBLIC ryml-_c4core_dev)
 endfunction()
+
+
+set(_ryml_ext_dir ${CMAKE_CURRENT_LIST_DIR})  # see https://stackoverflow.com/questions/12802377/in-cmake-how-can-i-find-the-directory-of-an-included-file
+
 
 function(_ryml_add_c4core_to_target target whichc4core doinstall)
     set(_listfile ${_ryml_ext_dir}/${whichc4core}.txt)
