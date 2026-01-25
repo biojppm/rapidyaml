a6c0fad0a0ab7a013ebe45735feb1db8ad3d2bfa
Joao Paulo Magalhaes <dev@jpmag.me>
2026-01-25 00:08:36 +0000

    wip

--- ext/c4core.cmake
+++ ext/c4core.cmake
@@ -10,16 +10,20 @@ endfunction()
 function(ryml_add_c4core_dev_to_target target)
     if(NOT TARGET ryml-_c4core_dev)
         c4_add_library(ryml-_c4core_dev LIBRARY_TYPE STATIC)
+message(STATUS "wtf1: ${_ryml_ext_dir}")
         _ryml_add_c4core_to_target(ryml-_c4core_dev c4core.dev OFF)
         target_include_directories(ryml-_c4core_dev PUBLIC
             ${_ryml_ext_dir}/c4core.src
             ${_ryml_ext_dir}/c4core.dev
         )
+        if(BUILD_SHARED_LIBS)
+            set_target_properties(ryml-_c4core_dev PROPERTIES
+                POSITION_INDEPENDENT_CODE ON)
+        endif()
         target_link_libraries(ryml-_c4core_dev PUBLIC ryml)
     endif()
     target_link_libraries(${target} PUBLIC ryml-_c4core_dev)
 endfunction()
-
 
 set(_ryml_ext_dir ${CMAKE_CURRENT_LIST_DIR})  # see https://stackoverflow.com/questions/12802377/in-cmake-how-can-i-find-the-directory-of-an-included-file
 
@@ -28,7 +32,7 @@ function(_ryml_add_c4core_to_target target whichc4core doinstall)
     set(_listfile ${_ryml_ext_dir}/${whichc4core}.txt)
     set(_srcdir ${_ryml_ext_dir}/${whichc4core})
     file(STRINGS ${_listfile} _src)
-    _c4_transform_to_full_path(_src _ ${_srcdir})
+    _c4_transform_to_full_path(_src _ "${_srcdir}")
     target_sources(${target} PRIVATE ${_src})
     #create_source_group("" "${_srcdir}" "${_src}")
     if(${doinstall})
