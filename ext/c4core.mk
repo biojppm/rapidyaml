2dae956f008cc3b5e71bb9ff408d29629dd24d2d
Joao Paulo Magalhaes <dev@jpmag.me>
2026-03-26 20:15:49 +0000

    wip

--- ext/c4core.mk
+++ ext/c4core.mk
@@ -2,4 +2,4 @@
 
 C4CORE_REPO := https://github.com/biojppm/c4core
 # must be tag or a FULL commit hash; cannot be a branch name.
-C4CORE_TAG := 897cf0a4961cddc2feacb76559534cc257259a42
+C4CORE_TAG := v0.11.1
